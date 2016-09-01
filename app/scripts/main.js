fe83c37f3a1fd780a2dd7fc8e88c3d33ea94d713
Jesse Ocon <jesse@masterclass.com>
2016-09-01 13:04:07 -0700

    Added Centering of Carousel Thumbs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,32 +1,45 @@
-var element = document.createElement('link');
-element.setAttribute('rel', 'stylesheet');
-element.setAttribute('type', 'text/css');
-element.setAttribute('href', 'https://s3-us-west-2.amazonaws.com/static.masterclass.com/optimizely-tests/video-carousel/main.css');
-document.getElementsByTagName('head')[0].appendChild(element);
+var ss = document.createElement('link');
+ss.type = 'text/css';
+ss.rel = 'stylesheet';
+ss.href = 'https://s3-us-west-2.amazonaws.com/static.masterclass.com/optimizely-tests/video-carousel/main.css';
+//ss.href= '../styles/main.css'
+document.getElementsByTagName('head')[0].appendChild(ss);
+
 window.MC = window.MC || {};
 window.MC.videoCarousel = {
   ATTRS_ARRAY: [
     {
-      id: 'uijym1gexz',
+      //id: 'uijym1gexz',
+      id: '9d37hjiund',
       thumbUrl: 'https://embed-ssl.wistia.com/deliveries/446a52bc8a7814b33ca8833adbe050e594831604.jpg?image_crop_resized=300x220',
+      //thumbUrl: '../images/320x180.png',
       title: 'Character Intro'
     },
     {
-      id: 'nlv3phbo9y',
+
+      //id: 'nlv3phbo9y',
+      id: '4za81shwpy',
       thumbUrl: 'https://embed-ssl.wistia.com/deliveries/d5a19e21b222e51424228d40c411c7b30a6f0386.jpg?image_crop_resized=300x220',
+      //thumbUrl: '../images/320x180.png',
       title: 'First 15 Pages'
     },
     {
-      id: '2o1eb9bgbs',
+      //id: '2o1eb9bgbs',
+      id: '2kdh6at4u5',
       thumbUrl: 'https://embed-ssl.wistia.com/deliveries/eefdddb24f17bbfcff215841479f7ea3f481afa7.jpg?image_crop_resized=300x220',
+      //thumbUrl: '../images/320x180.png',
       title: 'Leading the Audience'
     },
     {
-      id: '5jodpnn4x2',
+      //id: '5jodpnn4x2',
+      id: '1p82pinv8t',
       thumbUrl: 'https://embed-ssl.wistia.com/deliveries/8f6945871f55844577d62b044c45541e55306d35.jpg?image_crop_resized=300x220',
+      //thumbUrl: '../images/320x180.png',
       title: 'Intention and Obstacle'
     }
   ],
+
+
   currentVideoIndex: 0,
   updateVideoIndexCounter: function(idx) {
     if(idx === undefined || idx === null) {
@@ -46,19 +59,17 @@ window.MC.videoCarousel = {
     // idx may be undefined or null and it will simply increment
     var vidIndex = MC.videoCarousel.updateVideoIndexCounter(idx)
     var hashedId = MC.videoCarousel.ATTRS_ARRAY[vidIndex].id;
-    if (MC.videoCarousel.isOnline()) {
-      MC.videoCarousel.videoPlayer.replaceWith(hashedId, { transition: 'fade' });
-      MC.videoCarousel.videoPlayer.play();
-      $('body').trigger('mcVideoCarouselAdvanced', { currentVideo: { videoIndex: vidIndex, hashedId: hashedId}});
-      return MC.videoCarousel.videoPlayer;
-    } else {
-      $('body').trigger('mcVideoCarouselAdvanced', { currentVideo: { videoIndex: vidIndex, hashedId: hashedId}});
-    }
+    MC.videoCarousel.videoPlayer.replaceWith(hashedId, { transition: 'fade' });
+    MC.videoCarousel.videoPlayer.play();
+    $('body').trigger('mcVideoCarouselAdvanced', { currentVideo: { videoIndex: vidIndex, hashedId: hashedId}});
+    return MC.videoCarousel.videoPlayer;
   },
 
   endVideoHandler: function() {
     MC.videoCarousel.advanceVideo();
+    $('body').trigger('wistiaVideoEnded');
   },
+
 
   ready: function() {
     var SELECTORS, carouselHtml, thumbHtml, modalHtml;
@@ -93,30 +104,29 @@ window.MC.videoCarousel = {
 
     MC.videoCarousel.videoPlayers = [];
 
+    window._wq.push({'_all': function(videoPlayer){
+      MC.optimizelyVideoPlayer = videoPlayer;
+      MC.videoCarousel.videoPlayer = videoPlayer;
 
-    window._wq.push({ '_all': function(videoPlayer) {
-      //if (MC.videoCarousel.videoPlayer === null || MC.videoCarousel.videoPlayer === undefined) {
-        //MC.optimizelyVideoPlayer = videoPlayer;
-        //MC.videoCarousel.videoPlayer = videoPlayer;
-      //}
-      //MC.optimizelyVideoPlayer.bind('end', MC.videoCarousel.endVideoHandler);
-      //$('body').trigger('mcVideoCarouselVideoLoaded', { currentVideo: { videoIndex: 0, hashedId: videoPlayer.hashedId() }});
+      MC.optimizelyVideoPlayer.bind('end', MC.videoCarousel.endVideoHandler);
+      MC.optimizelyVideoPlayer.bind('play', function() {
+        $('body').trigger('wistiaVideoPlaying')
+      });
+
+      MC.optimizelyVideoPlayer.bind('pause', function() {
+        $('body').trigger('wistiaVideoPaused')
+      });
+
+      $('body').trigger('mcVideoCarouselVideoLoaded', { currentVideo: { videoIndex: 0, hashedId: videoPlayer.hashedId() }});
+
     }});
 
-    window._wq.push(
-        { 'uijym1gexz': function(videoPlayer) {
-          if (MC.videoCarousel.videoPlayer === null || MC.videoCarousel.videoPlayer === undefined) {
-            MC.optimizelyVideoPlayer = videoPlayer;
-            MC.videoCarousel.videoPlayer = videoPlayer;
-          }
-          var obj = { 'id': 'uijym1gexz', videoPlayer: videoPlayer };
-          MC.videoCarousel.videoPlayers.push(obj);
-          videoPlayer.bind('end', MC.videoCarousel.endVideoHandler)
-          $('body').trigger('mcVideoCarouselVideoLoaded', { currentVideo: { videoIndex: 0, hashedId: videoPlayer.hashedId() }});
-        }
-    });
 
 
+                                   //<script charset=\'ISO-8859-1\' src=\'//fast.wistia.com/assets/external/E-v1.js\' async></script>\
+                                   //<div class=\'wistia_embed wistia_async_uijym1gexz playlistLinks=auto videoFoam=true\' style=\'height:360px;width:640px\'>&nbsp;</div>\
+                               //<img src="https://s3-us-west-2.amazonaws.com/static.masterclass.com/optimizely-tests/video-carousel/images/close.png" class="close-button right" data-dismiss="modal" />\
+                               //<img src="./images/close.png" class="close-button right" data-dismiss="modal" />\
 
     modalHtml = '\
                  <div class=\'modal fade\' id=\'videoModal\' tabindex=\'-1\' role=\'dialog\' aria-labelledby=\'videoModalLabel\'>\
@@ -127,6 +137,7 @@ window.MC.videoCarousel = {
                            <div class=\'row\'>\
                              <div class=\'col-xs-12\'>\
                                <h3 class=\'section-title\'>CLASS PREVIEW</h3>\
+                               <img src="https://s3-us-west-2.amazonaws.com/static.masterclass.com/optimizely-tests/video-carousel/images/close.png" class="close-button right" data-dismiss="modal" />\
                              </div><!-- col-xs-12 -->\
                            </div><!-- row -->\
                          </div><!-- container -->\
@@ -140,12 +151,16 @@ window.MC.videoCarousel = {
                                    <script charset=\'ISO-8859-1\' src=\'//fast.wistia.com/assets/external/E-v1.js\' async></script>\
                                    <div class=\'wistia_embed wistia_async_uijym1gexz playlistLinks=auto videoFoam=true\' style=\'height:360px;width:640px\'>&nbsp;</div>\
                                  </div><!-- col-xs-12 -->\
-                                 <div class=\'col-xs-8\'>\
-                                   <h3 data-main-video-title=\'true\'>INTRODUCTION - FOCUS AND DESIRE</h3>\
-                                 </div><!-- col-xs-12 -->\
-                                 <div class=\'col-xs-4\'>\
-                                   <button href=\'#\' class=\'cta-button right\'>take the class</button>\
-                                 </div><!-- col-xs-6 -->\
+                                 <div class="header-wrapper">\
+                                   <div class="col-xs-8">\
+                                     <h3 data-main-video-title="true" style="">INTRODUCTION - FOCUS AND DESIRE</h3>\
+                                   </div><!-- col-xs-12 -->\
+                                   <div class="col-xs-4">\
+                                     <a href="#cart?product_id=16" class="cta-button right btn btn-lg" style="" id="ttc-modal">\
+                                       take the class\
+                                     </a>\
+                                   </div><!-- col-xs-4 -->\
+                                 </div><!-- header-wrapper -->\
                                  <div class=\'col-xs-12\'>\
                                    <hr></hr>\
                                  </div><!-- col-xs-12 -->\
@@ -167,7 +182,7 @@ window.MC.videoCarousel = {
     }
 
     carouselHtml = '\
-                    <div class=\'video-carousel container\' id=\'vid-2\'> \
+                    <div class=\'video-carousel\' id=\'vid-2\'> \
                       <div class=\'video-thumbs-container\'></div> \
                     </div> \
                     ';
@@ -244,14 +259,59 @@ window.MC.videoCarousel = {
       MC.videoCarousel.advanceVideo($this.data('video-index'));
     });
 
+    $('body').on('wistiaVideoPlaying', function() {
+      $('#play-button').fadeOut('fast');
+    });
+
+    $('body').on('wistiaVideoPaused', function() {
+      $('#play-button').fadeIn('fast');
+    })
+
+    $('body').on('wistiaVideoEnded', function() {
+      $('#play-button').fadeIn('fast');
+    });
+
+    $('body').on('click', '#play-button', function() {
+      MC.videoCarousel.videoPlayer.play()
+    })
+
     updateUIForCurrentVideo($($(SELECTORS.DATA_ATTRS.VIDEO_THUMB)[0]));
+
+    $('#ttc-modal').on('click', function(e){
+      if (!(MC.isLoggedIn() === true || gon.featherCartEnabled)) {
+        e.preventDefault();
+        MC.courseMarketing.setRequestedPath(e);
+        MC.courseMarketing.openCreateModal();
+      }
+    });
+
+
+    var georgeCloney = $('.hero-uniter').clone();
+    $('.hero-uniter').replaceWith(georgeCloney);
+
+    $('#play-hero-trailer').unbind();
+    $('#play-hero-trailer').on('click', function(e) {
+      $('#videoModal').modal('show');
+    });
+
+    $('#videoModal').on('shown.bs.modal', function(e) {
+      MC.videoCarousel.videoPlayer.play();
+    });
+
+    $('#videoModal').on('shown.bs.modal', function(e) {
+      var thumbsWidth = ($('.video-image-thumb-wrapper').length * $('.video-image-thumb-wrapper').outerWidth(true) - 30);
+      $('.video-image-thumb-wrapper').parent().css('width', thumbsWidth);
+    });
+
   },
 };
 
 window._wq = window._wq || [];
 _wq.push({ '_all': {
   videoFoam: true,
-  playerColor: 'ff0000',
+  playerColor: '000000',
+  playButton: 'false',
+  controlsVisibleOnLoad: 'false'
 }});
 
 $(document).ready(MC.videoCarousel.ready);
